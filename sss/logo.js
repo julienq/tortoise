47913abf92e04651fcfbb829d4c7c49e46376445
Julien Quint <julien@igel.co.jp>
2012-09-06 19:46:17 +0900

    Drawing a bit

--- sss/logo.js
+++ sss/logo.js
@@ -25,18 +25,50 @@
     forward: function (dist) {
       this.cx += dist * Math.cos(this.th);
       this.cy -= dist * Math.sin(this.th);
-      if (this.down) {
-        this.context.lineTo(this.cx, this.cy);
-        this.context.clearRect(this.context.canvas.width,
-            this.context.canvas.height);
-        this.context.stroke();
-      }
+      this.moved();
     },
 
     turn: function (dh) {
       var h = radtoh(this.th) + dh;
       this.th = htorad((360 + h) % 360);
+    },
+
+    get_x: function () {
+      return this.cx - this.context.canvas.width / 2;
+    },
+
+    get_y: function () {
+      return this.cy + this.context.canvas.height / 2;
+    },
+
+    get_h: function () {
+      return radtoh(this.th);
     }
+
+    set_x: function (x) {
+      this.cx = this.context.canvas.width / 2 + x;
+      this.moved();
+    },
+
+    set_y: function (y) {
+      this.cy = this.context.canvas.height / 2 - y;
+      this.moved();
+    },
+
+    set_h: function (h) {
+      this.th = htorad(h);
+    },
+
+    moved: function () {
+      if (this.down) {
+        this.context.lineTo(this.cx, this.cy);
+        this.context.clearRect(this.context.canvas.width,
+            this.context.canvas.height);
+        this.context.stroke();
+      } else {
+        this.context.moveTo(this.cx, this.cy);
+      }
+    },
   };
 
   logo.turtle = function (canvas) {
@@ -49,6 +81,16 @@
     env.bk = env.back = function (dist) { t.forward(-dist); };
     env.lt = env.left = function (dh) { t.turn(-dh); };
     env.rt = env.right = t.turn.bind(t);
+    env.setx = t.set_x.bind(t);
+    env.sety = t.set_y.bind(t);
+    env.setxy = function (x, y) { t.set_x(x); t.set_y(y); };
+    env.setpos = function (pos) { t.set_x(pos[0]); t.set_y(pos[1]); };
+    env.seth = env.setheading = t.set_h.bind(t);
+    env.home = function () { t.set_x(0); t.set_y(0); t.set_h(0); };
+    env.pos = function () { return [t.get_x(), t.get_y()]; };
+    env.xcor = t.get_x.bind(t);
+    env.ycor = t.get_y.bind(t);
+    env.heading = t.get_h.bind(t);
   };
 
 }(window.logo = {}));
